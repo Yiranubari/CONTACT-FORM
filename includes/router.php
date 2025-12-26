5c909b990f8d469fb8a78ea917c067902912cd66
Yiranubari <yiranubari4@gmail.com>
2025-12-26 14:39:26 +0100

    add normalizeUri function to router.php for URI normalization

--- includes/router.php
+++ includes/router.php
@@ -2,9 +2,19 @@
 
 declare(strict_types=1);
 
+function normalizeUri(string $uri): string
+{
+    $uri = strtolower(trim($uri, '/'));
+
+    return $uri === '' ? 'index' : $uri;
+}
+
+
+
 function dispatch(string $uri, string $method): void
 {
     //  1.) normalize the URI: GET /guestbook -> routes/guestbook_get.php
+
     // 2.) if !GET or POST - return 404
     // 3.) file path - PHP file path
     // 4.) if file exists, if not 404
