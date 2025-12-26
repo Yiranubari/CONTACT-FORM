8b24817ec15432915c90319549ec87e2deffac53
Yiranubari <yiranubari4@gmail.com>
2025-12-26 14:35:35 +0100

    add dispatch function to router.php for URI handling

---
+++ includes/router.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+function dispatch(string $uri, string $method): void
+{
+    //  1.) normalize the URI: GET /guestbook -> routes/guestbook_get.php
+    // 2.) if !GET or POST - return 404
+    // 3.) file path - PHP file path
+    // 4.) if file exists, if not 404
+    // 5.) if file exists, handle the route by including the PHP file
+}
