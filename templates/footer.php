5e964d9f8c9f7ff2aee8a59dc27ea5a64ceea8be
Yiranubari <yiranubari4@gmail.com>
2026-01-11 16:09:30 +0100

    add: create index_get.php and footer.php templates for contact form

---
+++ templates/footer.php
@@ -0,0 +1,7 @@
+ <footer>
+     <div class="container">
+         <p>&copy; <?php echo date("Y"); ?> Yiranubari Maamaa Promise. All rights reserved.</p>
+     </div>
+ </footer>
+
+ </body>
---
+++ templates/index_get.php
@@ -0,0 +1,11 @@
+ <main class="container">
+     <div class="card">
+         <h2>Hello</h2>
+         <p>Welcome</p>
+         <a href="about.php" class="cta-button">Learn more about me</a>
+     </div>
+ </main>
+
+
+
+ </html>
