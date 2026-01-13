aa33937403b19639978765fc1089481b109c08f7
Yiranubari <yiranubari4@gmail.com>
2026-01-13 04:09:39 +0100

    fix: remove duplicate name input field in contact form template

--- templates/contact_get.php
+++ templates/contact_get.php
@@ -1,9 +1,9 @@
 <section>
     <h2>Leave a Public Note/Question</h2>
     <form method="POST">
-
         <label>Name</label>
         <input type="text" name="name" />
-
+        <label>Name</label>
+        <input type="text" name="name" />
     </form>
 </section>
