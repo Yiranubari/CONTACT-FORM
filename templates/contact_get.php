1389e80d57da8d92031f59df39e03411b3dd6461
Yiranubari <yiranubari4@gmail.com>
2026-01-13 04:10:06 +0100

    fix: correct duplicate label and input field for email in contact form

--- templates/contact_get.php
+++ templates/contact_get.php
@@ -3,7 +3,7 @@
     <form method="POST">
         <label>Name</label>
         <input type="text" name="name" />
-        <label>Name</label>
-        <input type="text" name="name" />
+        <label>Email</label>
+        <input type="email" name="email" />
     </form>
 </section>
