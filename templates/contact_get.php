3882ab2e9b529332d02b9732d2899f1f3723e26b
Yiranubari <yiranubari4@gmail.com>
2026-01-13 04:11:40 +0100

    fix: add missing id attributes to input fields in contact form

--- templates/contact_get.php
+++ templates/contact_get.php
@@ -2,8 +2,11 @@
     <h2>Leave a Public Note/Question</h2>
     <form method="POST">
         <label>Name</label>
-        <input type="text" name="name" />
+        <input type="text" id="name" name="name" />
         <label>Email</label>
-        <input type="email" name="email" />
+        <input type="email" id="email" name="email" />
+        <label>Message</label>
+        <textarea rows="4" id="message" name="message"></textarea>
+
     </form>
 </section>
