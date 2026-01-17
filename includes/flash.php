b298a3ab09e6022873c61d14906038d383929530
Yiranubari <yiranubari4@gmail.com>
2026-01-17 14:51:59 +0100

    chore: implement flash message functions in flash.php for user notifications

---
+++ includes/flash.php
@@ -0,0 +1,13 @@
+<?php
+
+function addFlashMessage(string $type, string $message): void
+{
+    $_SESSION['flash'][$type][] = $message;
+}
+
+function getFlashMessages(): array
+{
+    $messages = $_SESSION['flash'] ?? [];
+    unset($_SESSION['flash']);
+    return $messages;
+}
