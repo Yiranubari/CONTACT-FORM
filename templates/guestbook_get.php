10216e8321eabd59fc49b9a257623f4e738936c8
Yiranubari <yiranubari4@gmail.com>
2026-01-17 12:24:24 +0100

    chore: add conditional display for guest messages in guestbook_get.php

--- templates/guestbook_get.php
+++ templates/guestbook_get.php
@@ -7,4 +7,10 @@ var_dump($messages);
     <h2>
         Guest Messages
     </h2>
+    <?php if (empty($messages)): ?>
+        <p>No messages yet. Be the first to leave a message!</p>
+    <?php else: ?>
+        <?php foreach ($messages as $messages): ?>
+        <?php endforeach; ?>
+    <?php endif; ?>
 </section>
