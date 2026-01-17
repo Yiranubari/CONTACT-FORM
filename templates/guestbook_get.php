bdea45974eb550edb78d4ad4edf48a3e2eff05c2
Yiranubari <yiranubari4@gmail.com>
2026-01-17 12:24:46 +0100

    chore: fix indentation and variable naming in guestbook_get.php for clarity

--- templates/guestbook_get.php
+++ templates/guestbook_get.php
@@ -8,9 +8,10 @@ var_dump($messages);
         Guest Messages
     </h2>
     <?php if (empty($messages)): ?>
-        <p>No messages yet. Be the first to leave a message!</p>
+    <p>No messages yet. Be the first to leave a message!</p>
     <?php else: ?>
-        <?php foreach ($messages as $messages): ?>
-        <?php endforeach; ?>
+    <?php foreach ($messages as $message): ?>
+
+    <?php endforeach; ?>
     <?php endif; ?>
 </section>
