9e55454487b3fbb308a1498706047d3c99d57ebc
Yiranubari <yiranubari4@gmail.com>
2026-01-17 14:58:57 +0100

    chore: format flash.php for better readability and structure

--- templates/flash.php
+++ templates/flash.php
@@ -1,1 +1,13 @@
-<?php $messages = getFlashMessages();
+<?php $messages = getFlashMessages(); ?>
+
+<?php if (!empty($messages)): ?>
+    <div class="flash-messages">
+        <?php foreach ($messages as $type => $msgs): ?>
+            <?php foreach ($msgs as $msg): ?>
+                <div class="flash-message flash-<?= htmlspecialchars($type) ?>">
+                    <?= htmlspecialchars($msg) ?>
+                </div>
+            <?php endforeach; ?>
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
