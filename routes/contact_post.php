af3768c318a5e898d1c932b3d1489fb9247070d7
Yiranubari <yiranubari4@gmail.com>
2026-01-17 15:13:39 +0100

    chore: replace success message echo with flash message in contact_post.php

--- routes/contact_post.php
+++ routes/contact_post.php
@@ -23,8 +23,7 @@ $inserted = insertMessage(
 
 if ($inserted) {
     $safeName = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
-    echo "Thank you, $safeName, for your message!";
-    exit;
+    addFlashMessage('success', 'Your message has been sent successfully!');
 }
 
 serverError("Could not save your message. Please try again later.");
