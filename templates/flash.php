a53ee26e152b8cc00f537f3a32f7c9ff589b213d
Yiranubari <yiranubari4@gmail.com>
2026-01-17 15:00:41 +0100

    chore: fix variable naming in flash.php for consistency

--- templates/flash.php
+++ templates/flash.php
@@ -1,13 +1,13 @@
 <?php $messages = getFlashMessages(); ?>
 
 <?php if (!empty($messages)): ?>
-    <div class="flash-messages">
-        <?php foreach ($messages as $type => $msgs): ?>
-            <?php foreach ($msgs as $msg): ?>
-                <div class="flash-message flash-<?= htmlspecialchars($type) ?>">
-                    <?= htmlspecialchars($msg) ?>
-                </div>
-            <?php endforeach; ?>
-        <?php endforeach; ?>
+<div class="flash-messages">
+    <?php foreach ($messages as $type => $message): ?>
+    <?php foreach ($msgs as $msg): ?>
+    <div class="flash-message flash-<?= htmlspecialchars($type) ?>">
+        <?= htmlspecialchars($msg) ?>
     </div>
+    <?php endforeach; ?>
+    <?php endforeach; ?>
+</div>
 <?php endif; ?>
